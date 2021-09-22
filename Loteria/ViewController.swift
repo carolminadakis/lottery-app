253eae07b4dfbeae061312d80a27eee354a0eff6
Ana Carolina Minadakis <carolminadakis@gmail.com>
2021-09-22 11:07:43 -0300

    Initial Commit

---
+++ Loteria/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Loteria
+//
+//  Created by Ana Carolina Bernardes Minadakis on 22/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
